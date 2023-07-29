2729d7de5c8b2c4b8bd8e2eb0800735f52e362f0
tbiswal <tanmayatechs@gmail.com>
2023-07-29 13:51:51 +0530

    Add frontend and serverless code

---
+++ serverless/index.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT,
+    database : process.env.RDS_DATABASE
+  });
+exports.handler = (event, context, callback) => {
+    // allows for using callbacks as finish/error-handlers
+    context.callbackWaitsForEmptyEventLoop = false;
+    const sql = "select * from PSD";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      callback(null, result)
+    });
+  };
